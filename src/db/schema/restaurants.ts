72400cc136943c25ba81155c9acccacb747a822d
Leonardo Cunha <lalmcunha@gmail.com>
2024-03-16 15:18:37 -0300

    Cria relacionamento e novas migrations

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,10 +1,32 @@
 import { createId } from "@paralleldrive/cuid2";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { users } from "./users";
+import { relations } from "drizzle-orm";
 
-export const restaurant = pgTable("restaurants", {
+export const restaurants = pgTable("restaurants", {
     id: text("id").$defaultFn(createId).primaryKey(),
     name: text("name").notNull(),
     description: text("description"),
+    // Relacionamento no BD
+    managerId: text('manager_id').references(() => users.id, {
+        onDelete: 'set null',
+    }),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
 })
+
+// Apenas para Drizzle
+export const restaurantsRelations = relations(restaurants, ({one}) => {
+    return {
+        manager: one(users, {
+            fields: [restaurants.managerId],
+            references: [users.id],
+            relationName: 'restaurant_manager'
+        })
+    }
+})
+
+/**
+ * É possível criar o relacionamento do outro lado também, mas só é necessário
+ * se a aplicação precisar dessa consulta
+ */
