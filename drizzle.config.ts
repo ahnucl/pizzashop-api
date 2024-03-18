614357e88c01ce586f6f76022ddc8fe1e262cc02
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-03-18 10:20:14 -0300

    Cria migraçẽs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default {
     schema: './src/db/schema/index.ts',
     out: './drizzle',
     driver: 'pg',
+    dbCredentials: {
+        connectionString: 'postgresql://postgres:docker@localhost:5432/pizzashop_dev',
+    }
 } satisfies Config
