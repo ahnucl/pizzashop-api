882fd102f4d6f995565aacce6e727709e47886b2
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-03-20 09:21:55 -0300

    Configura e aplica eslint

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,13 +1,13 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
-import chalk from "chalk";
-import { env } from "../env";
+import chalk from 'chalk'
+import { env } from '../env'
 
 const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
-await migrate(db, {migrationsFolder: 'drizzle'})
+await migrate(db, { migrationsFolder: 'drizzle' })
 console.log(chalk.greenBright('Migrations applied successfully!'))
 
 await connection.end()
--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,29 +1,29 @@
-import { createId } from "@paralleldrive/cuid2";
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-import { users } from "./users";
-import { relations } from "drizzle-orm";
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { users } from './users'
+import { relations } from 'drizzle-orm'
 
-export const restaurants = pgTable("restaurants", {
-    id: text("id").$defaultFn(createId).primaryKey(),
-    name: text("name").notNull(),
-    description: text("description"),
-    // Relacionamento no BD
-    managerId: text('manager_id').references(() => users.id, {
-        onDelete: 'set null',
-    }),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+export const restaurants = pgTable('restaurants', {
+  id: text('id').$defaultFn(createId).primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
+  // Relacionamento no BD
+  managerId: text('manager_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
 
 // Apenas para Drizzle
-export const restaurantsRelations = relations(restaurants, ({one}) => {
-    return {
-        manager: one(users, {
-            fields: [restaurants.managerId],
-            references: [users.id],
-            relationName: 'restaurant_manager'
-        })
-    }
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.managerId],
+      references: [users.id],
+      relationName: 'restaurant_manager',
+    }),
+  }
 })
 
 /**
