14be1a37c24704ba1d1779cf47b917a4d8cbe8a8
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-04-05 09:44:39 -0300

    Cria relacionamentos e migration

---
+++ drizzle/0006_aberrant_blackheart.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_order_id_users_id_fk";
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_orders_id_fk" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,6 +1,6 @@
 import { createId } from '@paralleldrive/cuid2'
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
-import { users } from './users'
+import { orders, products, users } from '.'
 import { relations } from 'drizzle-orm'
 
 export const restaurants = pgTable('restaurants', {
@@ -16,13 +16,15 @@ export const restaurants = pgTable('restaurants', {
 })
 
 // Apenas para Drizzle
-export const restaurantsRelations = relations(restaurants, ({ one }) => {
+export const restaurantsRelations = relations(restaurants, ({ one, many }) => {
   return {
     manager: one(users, {
       fields: [restaurants.managerId],
       references: [users.id],
       relationName: 'restaurant_manager',
     }),
+    orders: many(orders),
+    products: many(products),
   }
 })
 
